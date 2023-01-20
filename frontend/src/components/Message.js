d30c50d2ce7d83fa3eb1ca3eb953cc96241f1f61
Niaj Mohammad <niaj.1988@gmail.com>
2023-01-21 00:46:07 +0600

    Update Message.js

--- frontend/src/components/Message.js
+++ frontend/src/components/Message.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Alert } from 'react-bootstrap'
 
-const Message = ({ variant, children }) => {   // Passing two Props-> variant and children
+const Message = ({ variant, children }) => {   // Passing two Props-> variant and children here
     return (
         <Alert variant={variant}>
             {children}
